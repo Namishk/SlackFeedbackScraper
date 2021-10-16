5e3d7c5c66af3b03ddac6be37fe35196503b2d39
namishk <namishkhandelwal1@gmail.com>
2021-10-16 10:01:49 +0530

    App gets webpage code.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+class Scraper{
+        static void Main(string[] args){
+            string getChannels = String.Format("https://app.slack.com/client/T02JN670FLY");
+
+            WebRequest reqObj = WebRequest.Create(getChannels);
+            reqObj.Method = "GET";
+            HttpWebResponse respObj = null;
+            respObj = (HttpWebResponse)reqObj.GetResponse();
+            string res = null;
+            using(Stream stream = respObj.GetResponseStream()){
+                StreamReader sr = new StreamReader(stream);
+                res = sr.ReadToEnd();
+                sr.Close();
+                Console.WriteLine(res);
+            }
+    }
+}
